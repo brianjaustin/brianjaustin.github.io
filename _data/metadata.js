7faeb0fe0f2eb2421b7c153433acfb142d7b0ef8
Brian Austin <brianjaustin@gmail.com>
2024-06-07 23:43:12 -0400

    initial rss/sitemap stuff

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "Brian's Blog",
+	url: "https://brianjaustin.github.io/",
+	language: "en",
+	description:
+		"Occasional thoughts about programming, human-friendly cities, and other random things.",
+	author: {
+		name: "Brian Austin",
+		url: "https://brianjaustin.github.io/about-me/",
+	},
+};
